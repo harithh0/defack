0b6fa39b0e59b59c32fa831e6925e61a9c46307d
harithh0 <169714068+harithh0@users.noreply.github.com>
2025-07-18 16:43:58 -0500

    fixed telnet impl

--- test_default_creds.py
+++ test_default_creds.py
@@ -1,7 +1,9 @@
+import os
 import socket
 import telnetlib
 
 import paramiko
+import pexpect
 
 
 def SSHLogin(host, port, username, password):
@@ -20,29 +22,34 @@ def SSHLogin(host, port, username, password):
 
 
 def TelnetLogin(host, port, username, password):
-    tn = telnetlib.Telnet(host, port, timeout=1)
-    tn.read_until(b"login: ")
+    tn = telnetlib.Telnet(host, port, timeout=10)
+
+    tn.read_until(b"login:")
     tn.write((username + "\n").encode("utf-8"))
-    tn.read_until(b"Password: ")
+    tn.read_until(b"Password:")
     tn.write((password + "\n").encode("utf-8"))
-    try:
-        result = tn.expect([b"Last login"])
-        if result[0] > 0:
-            print(
-                "Telnet login successful on %s:%s with username %s and password %s"
-                % (host, port, username, password))
-        tn.close()
-    except (EOFError, socket.timeout):
-        print("Telnet login failed %s %s" % (username, password))
+
+    failed_login_status = tn.read_until(b"Login incorrect", timeout=5)
+
+    # INFO: 'failed_login_status' can either return empty string with ' \r\n' (usually when its passed the timeout time) or 'Login incorrect' somewhere in the line
+    if (b"Login incorrect" in failed_login_status
+            or failed_login_status == " \r\n".encode("ascii")):
+        print(f"Telnet login failed {username}:{password}")
+    else:
+        print(f"Telnet successful {username}:{password}")
+        tn.write(b"exit")
+
+    # gracefully 'FIN' close the connection
+    tn.close()
 
 
 host = "10.0.0.212"
 sshport = 22
 telnetport = 23
-with open("defaults.txt", "r") as f:
+with open(os.path.join(os.path.dirname(__file__), "defaults.txt"), "r") as f:
     for line in f:
         vals = line.split()
         username = vals[0].strip()
         password = vals[1].strip()
-        SSHLogin(host, sshport, username, password)
+        # SSHLogin(host, sshport, username, password)
         TelnetLogin(host, telnetport, username, password)
