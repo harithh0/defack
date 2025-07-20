a362e6c194dec62a7e0d4c168176804911390c2b
harithh0 <169714068+harithh0@users.noreply.github.com>
2025-07-20 11:17:03 -0500

    implementing info extract

--- linux_account_auth_monitor.py
+++ linux_account_auth_monitor.py
@@ -23,18 +23,29 @@ line = "Failed password for invalid user zencji from 10.0.0.212 port 56026 ssh2"
 line2 = "Failed password for harith from 10.0.0.113 port 38188 ssh2"
 failed_user_re = re.compile(
     r"Failed password for (?:invalid user )?(\w+) from ([\d.]+) port (\d+)")
+accepted_user_re = re.compile(
+    r"Accepted password for (\w+) from ([\d.]+) port (\d+)")
 match = failed_user_re.search(line2)
+
+allowed_users = set("harith")
+allowed_ips = set("10.0.0.113")
 
 
 def extract_info(line: str, type: str) -> dict[str, str]:
     info = {}
     if type == "fail":
         match = failed_user_re.search(line)
-        if match:
-            info["user"] = match.group(1)
-            info["ip"] = match.group(2)
-            info["port"] = match.group(3)
+    elif type == "accepted":
+        match = accepted_user_re.search(line)
+    else:
+        return {}
 
+    if match:
+        info["user"] = match.group(1)
+        info["ip"] = match.group(2)
+        info["port"] = match.group(3)
+        if type == "fail":
+            info["is_invalid_user"] = "invalid user" in match.group(0)
     return info
 
 
@@ -43,8 +54,13 @@ def main():
         formatted_line = line.decode().strip()
         if sshd_re.search(formatted_line):
             if "Failed password for" in formatted_line:
-                info = extract_info(formatted_line, "fail")
-                print(info)
+                type = "fail"
+            elif "Accepted password for" in formatted_line:
+                type = "accepted"
+            else:
+                continue
+            info = extract_info(formatted_line, type)
+            print(info, type)
 
 
 try:
